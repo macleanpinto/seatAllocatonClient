8d3e97cb01b80f9776ab88bc6659e946b79d1ea7
adeshbedre <adeshbedre@gmail.com>
2018-08-07 06:21:11 +0530

    import seats components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+import { SeatsImportComponent } from './seats-import/seats-import.component';
 
 // AoT requires an exported function for factories
 export const createTranslateLoader = (http: HttpClient) => {
@@ -36,7 +37,7 @@ export const createTranslateLoader = (http: HttpClient) => {
         }),
         AppRoutingModule
     ],
-    declarations: [AppComponent],
+    declarations: [AppComponent, SeatsImportComponent],
     providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
