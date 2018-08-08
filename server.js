b08456e92a7e492bf091c49206d21b805f93fb68
Maclean Pinto <Maclean.Pinto@gfs.com>
2018-08-08 19:39:25 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000);
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
-console.log('Listening to ' + process.env.PORT);
+console.log("Express server listening on port %d", process.env.PORT);
