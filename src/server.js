6725a6e6c3851c22419284618a49ad4ebde9f310
Maclean Pinto <Maclean.Pinto@gfs.com>
2018-08-08 19:19:15 +0530

    Heroku deployment config

--- src/server.js
+++ src/server.js
@@ -7,3 +7,4 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
+console.log('Listening to ' + process.env.PORT);
